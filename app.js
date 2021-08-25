40e5727173395e5adbc14304673d8476b776907e
DEONTAY JEFFERSON <djefferson46@gmail.com>
2021-08-25 12:28:25 -0700

    Fixed varables

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Deontay";
 
 // Exercise 2
-
+let lastName = "Jefferson";
 // Exercise 3
+
+//age
+var age = 38;
+
+//fullName
+let fullName  =  firstName + " " + lastName;
+let fullName2 = `My name is ${firstName} ${lastName}`;
+
+//log to the console
+console.log(fullName);
+console.log();
+console.log(fullName2);
+
+
+
+
+//My Story
+let city = "Huntsville";
+let passTime = "Big Springs";
+let myStory = `Hello my name is ${fullName}. I live in the great city of ${city}, and my favorite thing to do here to walk at ${passTime} park. `
+
+console.log(myStory);
